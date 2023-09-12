6ed6da5f351f365a3f08dbd08abb2c7d398bb430
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-09-12 14:04:47 +0100

    Correction Jeu

--- Rappels-JS/ex-jeu.js
+++ Rappels-JS/ex-jeu.js
@@ -1,3 +1,23 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 // import d'un API (ici readline)
 const readline = require('node:readline');
 
@@ -11,18 +31,36 @@ function jouer() {
   // pose une question
   // - la question s'affiche de façon synchrone
   // - la réponse est attendue en asynchrone
-  rl.question('What do you think of Node.js? ', (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  }
 
-    // rejouer
-    jouer();
+  rl.question('Quel est le nombre ? ', (answer) => {
 
-    // arrête le programme :
-    // rl.close();
+    const entierSaisi = Number.parseInt(answer, 10)
+
+    if (!Number.isNaN(entierSaisi)) {
+      console.log('Erreur : il faut saisir un entier');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
 
+const entierAlea = getRandomIntInclusive(0, 100);
+const essais = [];
 jouer();
 
 // call stack
