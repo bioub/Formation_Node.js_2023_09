c8d99b9fbf8a85e8db4b24659d8a275f41e170c5
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-09-14 13:39:33 +0100

    Exemple Express

--- API-Node/14-http.js
+++ API-Node/14-http.js
@@ -4,9 +4,15 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Bonjour tout le monde');
+  if (req.method === 'GET' && req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Home');
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
 });
 
 
---
+++ API-Node/15-express.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const app = express();
+
+// app.use(express.json());
+
+app.post('/users/register', express.json(), (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    msg: 'user created',
+  })
+});
+
+// app.all quelque soit la méthode HTTP
+// et toutes les url qui comment par /api
+// app.use('/api', () => {
+//   res.send('/api');
+// });
+
+// app.all quelque soit la méthode HTTP
+// URL exacte /demo-all
+app.all('/demo-all', (req, res) => {
+  res.send('Demo');
+});
+
+// app.METHOD HTTP
+app.get('/', (req, res) => {
+  console.log(req.query);
+  res.send('Home');
+});
+
+app.get('/hello/:prenom', (req, res) => {
+  res.json({ msg: 'Hello ' + req.params.prenom });
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ msg: 'Hello' });
+});
+
+app.listen(3000, () => {
+  console.log(`Server running at http://127.0.0.1:3000/`);
+});
